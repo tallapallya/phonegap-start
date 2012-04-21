d4ab739f689a682bf88aa7a8d2d66b84bf77738b
tallapallya <dittu81@gmail.com>
2012-04-21 07:40:32 -0700

    Update assets/js/app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -46,7 +46,14 @@ run(function () {
             });
         });
     });
-    when('#save', function () {
+
+  when('#cam', function() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+   });
+
+  when('#save', function () {
         store.save({
             key:'config',
             map:ui('map'),
